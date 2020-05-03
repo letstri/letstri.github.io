f98147eda368d533f45ecfecce0c9723b565133c
BrooonS <brooons@yandex.ru>
2020-05-03 14:28:00 +0300

    add copyright

--- projects/filter-color-generator/src/FilterColorGenerator.vue
+++ projects/filter-color-generator/src/FilterColorGenerator.vue
@@ -75,6 +75,14 @@
         <!-- eslint-enable -->
       </div>
     </div>
+    <div class="copyright">
+      Main functions copied from
+      <a
+        href="https://stackoverflow.com/questions/42966641/how-to-transform-black-into-any-given-color-using-only-css-filters/43960991#43960991"
+        target="_blank"
+        rel="noopener noreferrer"
+      >stackoverflow answer</a>.
+    </div>
   </div>
 </template>
 
@@ -243,5 +251,11 @@ img {
 
 .vc-chrome {
   font-family: sans-serif !important;
+}
+
+.copyright {
+  font-size: 12px;
+  margin-top: 20px;
+  opacity: 0.5;
 }
 </style>
